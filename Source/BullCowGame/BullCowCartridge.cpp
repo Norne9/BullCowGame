1eeaccf5594fc6b647d6d52e894f2eb250602fa4
Norne9 <norne19@outlook.com>
2021-06-18 00:19:58 +0500

    Welcome message on terminal

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,13 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to game!"));
+    PrintLine(TEXT("Enter text: "));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
+    FString HiddenWord = TEXT("Word");
+    GEngine->AddOnScreenDebugMessage(-1, 1.5f, FColor::Cyan, Input);
 }
